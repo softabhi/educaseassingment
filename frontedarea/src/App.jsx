4f7936906eeb639a2e935a3b8a7d3808d77957e5
softabhi <99878096+softabhi@users.noreply.github.com>
2024-04-24 11:13:07 +0530

    first commit

---
+++ frontedarea/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+// import './App.css'
+import Login from './componets/login/Login'
+import Home from './componets/Home'
+import Register from './componets/registration/Register';
+import Profile from './componets/profile/Profile';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
